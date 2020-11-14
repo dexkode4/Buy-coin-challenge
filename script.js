6962ddd3a3db3c54dc8470a155a4ab83294ebd85
dexkode4 <dexkode4@gmail.com>
2020-11-14 03:03:49 +0100

    created navbar and profile images

---
+++ script.js
@@ -0,0 +1,92 @@
+const username = "dexkode4";
+const token = "2417ba3d2a643e25738439f7f14fa23d01e5617a";
+
+
+async function fetchGraphQL(text, variables) {
+    const GITHUB_AUTH_TOKEN = token;
+
+    // Fetch data from GitHub's GraphQL API:
+    const response = await fetch('https://api.github.com/graphql', {
+        method: 'POST',
+        headers: {
+            Authorization: `bearer ${GITHUB_AUTH_TOKEN}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query: text,
+            variables,
+        }),
+    });
+
+    return await response.json();
+}
+
+
+const url = "https://api.github.com/graphql";
+
+const query = `
+query { 
+    user(login: "${username}"){
+     avatarUrl
+     login
+     bio
+     
+     repositories(first: 20) {
+         nodes{
+           name
+           stargazerCount
+           updatedAt
+              languages(first:1){
+             nodes{
+               name
+             }
+           }
+           
+           forkCount
+           
+           }
+         }
+      }
+     }
+`;
+
+const opts = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+    },
+    body: JSON.stringify({ query: query })
+};
+
+const getMyGithubData = () => {
+
+    
+}
+
+fetchGraphQL(query).then(res => {
+    console.log(res.data);
+    const { avatarUrl, repositories } = res.data.user;
+  
+    let repoCount = document.getElementById('repo-count');
+    // set avatar img
+    document.getElementById('avatar').src = avatarUrl;
+    document.getElementById('profile-image').src = avatarUrl;
+    // set repo count
+    repoCount.textContent = repositories.nodes.length;
+
+
+}).catch(console.error);
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
